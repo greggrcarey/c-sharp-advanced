fcc09e43131232433f0f69d90fcc8f0ed63f5b3b
Gregg Carey <gregg.carey@outlook.com>
2022-04-04 10:56:58 -0500

    Create Delegates Invoke and Pass as parameters

--- WarehouseManagementSystem.Business/OrderProcessor.cs
+++ WarehouseManagementSystem.Business/OrderProcessor.cs
@@ -4,17 +4,32 @@ namespace WarehouseManagementSystem.Business
 {
     public class OrderProcessor
     {
+        //delegate declaration
+        public delegate void OrderInitialized();
+        public delegate void ProcessCompleted();
+
+        //This property can refernce ANY void method without paramters
+        //Not just the delegate we defined above
+        public OrderInitialized OnOrderInitialized { get; set; }
+
         private void Initialize(Order order)
         {
+            ArgumentNullException.ThrowIfNull(order);
+
+            OnOrderInitialized?.Invoke();
+            //Call the delegate through the property
+            //The delegate is a type, so it has its own properties also, even if it is null
         }
 
-        public void Process(Order order)
+        public void Process(Order order, ProcessCompleted onCompleted = default)
         {
             // Run some code..
 
             Initialize(order);
 
-            // How do I produce a shipping label?
+            //By passing the delegate as a parameter to the method,
+            //we are able to more tightly controll access to the delegate
+            onCompleted?.Invoke();
         }
     }
 }
--- WarehouseManagementSystem/Program.cs
+++ WarehouseManagementSystem/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using WarehouseManagementSystem.Business;
+using WarehouseManagementSystem.Domain;
+
+var order = new Order
+{
+    LineItems = new[]
+    {
+        new Item {Name = "PS1", Price = 50},
+        new Item {Name = "PS2", Price = 60},
+        new Item {Name = "PS4", Price = 70},
+        new Item {Name = "PS5", Price = 80}
+    }
+};
+
+var processor = new OrderProcessor
+{
+    OnOrderInitialized = SendMessageToWarehouse
+    //Point the delegate to the SendMessageToWarehouse method
+    //One "issue" with public delegates is that they can be invoked from anywhere
+};
+
+processor.Process(order, SendConfirmaitonEmail);
+
+void SendMessageToWarehouse()
+{
+    Console.WriteLine("Please pack the order");
+}
+
+void SendConfirmaitonEmail()
+{
+    Console.WriteLine("Order Confirmation Email");
+}
